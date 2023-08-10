bf20634fba61a39b1452eac69fa77b0e27b19687
nsaqib1 <nsaqib32@gmail.com>
2023-08-10 21:31:52 +0600

    module2 assignment

---
+++ module2/module2_assignment.dart
@@ -0,0 +1,22 @@
+void main() {
+  // Given grades
+  const List<int> grades = [85, 93, 78, 65, 88, 72];
+
+  // Calculating average grade
+  int totalGrade = 0;
+  for (int grade in grades) {
+    totalGrade += grade;
+  }
+  final double averageGrade = totalGrade / grades.length;
+  final int roundedAverageGrade = averageGrade.round();
+
+  // Printing result
+  print('Student\'s average grade: ${averageGrade.toStringAsFixed(2)}');
+  print('Rounded average: $roundedAverageGrade');
+
+  if (roundedAverageGrade >= 70) {
+    print('Passed');
+  } else {
+    print('Failed');
+  }
+}
