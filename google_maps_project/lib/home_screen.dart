d690a8920a7692f11492439165f7605adf9c183d
nsaqib1 <nsaqib32@gmail.com>
2023-12-23 13:34:34 +0600

    add animate to current position

--- google_maps_project/lib/home_screen.dart
+++ google_maps_project/lib/home_screen.dart
@@ -1,7 +1,8 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_project/utils/location_services.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,12 +12,53 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
+  late final GoogleMapController _mapController;
+  late final StreamSubscription _streamSubscription;
+  Position? _position;
 
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
+  static const CameraPosition _initialPosition = CameraPosition(
+    target: LatLng(23.7808186, 90.3372882),
+    zoom: 1,
   );
+
+  void _listenAndUpdateLocationOnMap() async {
+    // Getting Permission
+    try {
+      await LocationServices.getPermission();
+    } catch (e) {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            backgroundColor: Colors.red.shade900,
+            content: Text(
+              e.toString(),
+            ),
+          ),
+        );
+      }
+      return;
+    }
+
+    // Getting Current Position
+    _position = await LocationServices.getCurrentPosition();
+
+    _mapController.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: LatLng(_position?.latitude ?? 37.42796133580664, _position?.longitude ?? -122.085749655962),
+          zoom: 17,
+        ),
+      ),
+    );
+
+    // Updating location
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _streamSubscription.cancel();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,9 +67,10 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text("Real-Time Location Tracker"),
       ),
       body: GoogleMap(
-        initialCameraPosition: _kGooglePlex,
+        initialCameraPosition: _initialPosition,
         onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
+          _mapController = controller;
+          _listenAndUpdateLocationOnMap();
         },
       ),
     );
